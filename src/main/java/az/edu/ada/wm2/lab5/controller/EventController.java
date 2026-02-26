900e9d5a6baf39ca3ff642ba99ee94ea3922caff
AgilAliyev1 <aqilliyev207@gmail.com>
2026-02-26 15:30:13 +0400

    Add EventController:PatchMapping  implementation

--- src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
+++ src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
@@ -124,5 +124,23 @@ public class EventController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)
         }
 }
-
+@PatchMapping("/{id}/price")
+    public ResponseEntity<Event>updateEventPrice(
+            @PathVariable UUID id,
+            @RequestParam("price") BigDecimal price){
+        try{
+            Event updatedEvent = eventService.updateEventPrice(id, price);
+            return new ResponseEntity<>(updatedEvent, HttpStatus.OK);
+        }
+        catch (IllegalArgumentException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        catch (RuntimeException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+}
+)
 }
