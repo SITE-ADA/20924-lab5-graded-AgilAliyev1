9bca935d87afb93e4045b494a3622a379dcf42e5
AgilAliyev1 <aqilliyev207@gmail.com>
2026-02-26 15:22:18 +0400

    Add EventController:getMapping /filter/price implementation

--- src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
+++ src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
@@ -103,5 +103,17 @@ public class EventController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+    @GetMapping("/filter/price")
+    public ResponseEntity<List<Event>> getEventsByPriceRange(
+            @RequestParam("min") BigDecimal min,
+            @RequestParam("max") BigDecimal max) {
+        try {
+            List<Event> events = eventService.getEventsByPriceRange(min, max);
+            return new ResponseEntity<>(events, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
 
 }
